d93163a5674d547cdd0e211d83b93cc0bfa4deb2
BaradRonaksinh <baradronaksinh21303@gmail.com>
2024-11-18 13:42:54 +0530

    make webpage use of react hooks

---
+++ src/UsecontextEx.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UsecontextEx = () => {
+  return (
+    <div>
+      <h2>Use Context is Here</h2>
+    </div>
+  )
+}
+
+export default UsecontextEx
---
+++ src/UsenevigateExample.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UsenevigateExample = () => {
+  return (
+    <div>
+      <button>Add User</button>
+      <button>Display User</button>
+    </div>
+  )
+}
+
+export default UsenevigateExample
