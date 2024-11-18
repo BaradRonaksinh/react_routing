fcd433bd025e6a04b6b64127cd82f7eb9ae68d45
BaradRonaksinh <baradronaksinh21303@gmail.com>
2024-11-18 17:55:08 +0530

    learn lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,31 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Adduser from './Adduser'
 import Viewuser from './Viewuser'
 import Userinfo from './Userinfo'
+import { lazy, Suspense } from 'react'
+
+
+const Home = lazy(() => import('./Home'))
+const Contact = lazy(() => import('./Contact'))
 
 function App() {
 
 
   return (
     <>
-      
-        <BrowserRouter>
+    <Suspense>
+      <Home />
+    </Suspense>
+    <Suspense>
+      <Contact />
+    </Suspense>
+        {/* <BrowserRouter>   //Routing and navigate
           <Routes>
             <Route path='/' element={<UsenevigateExample />} />
             <Route path='/addUser' element={<Adduser />} />
             <Route path='/dispUser' element={<Viewuser />} />
             <Route path='/view/:id/:name' element={<Userinfo />} />
           </Routes>
-        </BrowserRouter>
+        </BrowserRouter> */}
       
     </>
   )
